84b99cbb64bc67fd28bf06f19eee32ad11612622
Varun Mathur <varunmathur1990@gmail.com>
2023-08-01 23:17:44 +0530

    Update Reddit_Scraper.py

--- Reddit_Scraper.py
+++ Reddit_Scraper.py
@@ -36,36 +36,34 @@ python cli_redditnews.py run --query 'icml 2023, neurips 2023'  --dirout ztnp/re
 
 """
 import fire, os, praw, pandas as pd
-from utilmy import (date_now, pd_to_file, os_makedirs, log)
+from datetime import date
 
-
-def run(query:str='icml 2023 ', dirout:str="ztmp/", subreddits=None, reddit_limit=10, reddit_sort='top', verbose=1):
+def run(query:str, dirout:str, subreddits=None, reddit_limit=10, reddit_sort='top'):
 
     ### from https://www.reddit.com/prefs/apps/
     client_id     = "KqVbVrlmGdbowtjuNIMAmQ" 
     client_secret = "XN4XRE7_pOw9rzreelTFYogmBTWW_g" 
-    user_agent    = "DailyNews"  ### Same from Secret page
+    user_agent    = "DailyNews" 
     reddit = praw.Reddit(client_id = client_id,#my client id
                      client_secret = client_secret,  #your client secret
                      user_agent    = user_agent #user agent name
                      )
-    log(reddit.read_only)
-    if verbose > 1 :
-        log(client_id, client_secret, user_agent)
 
     if subreddits is None :
-        subreddits = ['MachineLearning','OpenAI','ChatGPT','OpenAIDev','learnmachinelearning']
+        subreddits = ['AskReddit']
         
     if isinstance(query, str):
         query = query.split(",")
 
-    ymd = date_now(fmt='%Y%m%d', returnval='str')    
-
-    log("########## Start Scrapping")
+    ymd = date.today()    
+    ymd = today.strftime("%d/%m/%Y")
+    
+    print("########## Start Scrapping")
+    
     dfall = pd.DataFrame() 
     for s in subreddits:
         subreddit = reddit.subreddit(s) 
-        log('fetching:', s)
+        print('fetching:', s)
 
         for item in query:
             ddict = {
@@ -85,7 +83,7 @@ def run(query:str='icml 2023 ', dirout:str="ztmp/", subreddits=None, reddit_limi
             log( f'{item}: N article: ', len(dfres))
             dfall = pd.concat([dfall,dfres], axis=0,ignore_index=True)
 
-    pd_to_file(dfall, dirout + f"/subreddit_{ymd}.csv", sep="\t")
+    df.to_csv(os.path.join(dirout,"subreddit.csv"))
     return
 
 if __name__ == "__main__":
